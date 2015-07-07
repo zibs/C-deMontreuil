4e7df72ed767621c9c229043b8e4e19f884859b1
zibs <elizibin@gmail.com>
2015-07-06 20:24:35 -0700

    Redo entire site, add pages, fonts, htaccess, images

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
- $(function () { $('.expander').simpleexpand(); }); 	
+$(function () { $('.expander').simpleexpand(); }); 	
  
-
 function lightboxer () {
 document.getElementById('screen-show').click();
 
 }
+
+$(document).ready(function () {
+   $("#projects-bar").click(function () {
+    $(".projects").fadeToggle(1000);
+    $("#projects-bar").toggleClass("projectbold");
+	});
+});
+
+
+
+$(document).ready(function() {
+	$(".cv-download-box").hover(function() {
+		$(".fa-cloud-download").fadeToggle(500)
+	});
+});
