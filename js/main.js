01dfdc047b11ffff01ad856e90106bace62fe2d1
zibs <elizibin@gmail.com>
2015-07-10 16:06:01 -0700

    redesign slideshow

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-$(function () { $('.expander').simpleexpand(); }); 	
- 
-function lightboxer () {
-document.getElementById('screen-show').click();
-
-}
-
 $(document).ready(function () {
    $("#projects-bar").click(function () {
     $(".projects").fadeToggle(1000);
@@ -12,10 +5,113 @@ $(document).ready(function () {
 	});
 });
 
-
+// DOWNLOAD FADER
 
 $(document).ready(function() {
 	$(".cv-download-box").hover(function() {
 		$(".fa-cloud-download").fadeToggle(500)
 	});
 });
+
+// THUMBNAIL CHANGER
+$(document).ready(function() {
+$(".thumbnails img").click(function() {
+    $("#fullscreen").attr("src", $(this).attr("src"));
+	});
+});
+
+function nextImage() {
+
+					var current_image 			 = $("#fullscreen").attr("src");
+					var current_image_thumbnail  = $('.thumbnails img[src="'+current_image+'"]');
+					var next_image_thumbnail     = $(current_image_thumbnail).next();
+					
+
+					document.getElementById("fullscreen").src = next_image_thumbnail.attr("src");
+					$(".next, .prev").stop(true, true).fadeOut(10).stop(true, true);
+
+					
+					var right_edge_icon = $(".feature-image img").width() + $(".feature-image img").offset().left;
+					var left_edge_icon  = $(".feature-image img").offset().left;
+
+					
+					if (next_image_thumbnail.next().length) {				
+						$(".next").css({"left":+right_edge_icon+"px"});
+						var real_right_edge = right_edge_icon - ($(".next").width()/2);
+						$(".next").css({"left":+real_right_edge+"px"});
+						$(".next").stop(true, true).fadeIn(1000);
+					}
+
+					if (next_image_thumbnail.prev().length) {
+						$(".prev").css({"left":+left_edge_icon+"px"});
+						var real_left_edge = left_edge_icon - ($(".prev").width()) - 12;
+						$(".prev").css({"left":+real_left_edge+"px"});
+						$(".prev").stop(true, true).fadeIn(1000);
+					}
+}		
+
+function prevImage() {	
+
+					var current_image 			 = $("#fullscreen").attr("src");
+					var current_image_thumbnail  = $('.thumbnails img[src="'+current_image+'"]');
+					var prev_image_thumbnail     = $(current_image_thumbnail).prev();
+
+					document.getElementById("fullscreen").src = prev_image_thumbnail.attr("src");
+					$(".prev, .next").stop(true, true).fadeOut(10).stop(true, true);
+
+					var right_edge_icon = $(".feature-image img").width() + $(".feature-image img").offset().left;
+					var left_edge_icon  = $(".feature-image img").offset().left;
+
+					if (prev_image_thumbnail.prev().length) {
+						$(".prev").css({"left":+left_edge_icon+"px"});
+						var real_left_edge = left_edge_icon - ($(".prev").width()) - 12;
+						$(".prev").css({"left":+real_left_edge+"px"});
+						$(".prev").stop(true, true).fadeIn(400);
+
+					}
+					if (prev_image_thumbnail.next().length) {
+						$(".next").css({"left":+right_edge_icon+"px"});
+						var real_right_edge = right_edge_icon - ($(".next").width()/2);
+						$(".next").css({"left":+real_right_edge+"px"});
+						$(".next").stop(true, true).fadeIn(1000);
+					}
+}
+
+$(document).ready(function() {
+
+	$(".feature-image").mouseenter(function () {
+		var current_image 			 = $("#fullscreen").attr("src");
+		var current_image_thumbnail  = $('.thumbnails img[src="'+current_image+'"]');
+
+		var left_edge_icon  = $(".feature-image img").offset().left;
+
+		var right_edge_icon = $(".feature-image img").width() + $(".feature-image img").offset().left;
+
+		// NEXT
+		if ($(current_image_thumbnail).next().length) {
+			$(".next").css({"left":+right_edge_icon+"px"});
+			var real_right_edge = right_edge_icon - ($(".next").width()/2);
+			$(".next").css({"left":+real_right_edge+"px"});
+			$(".next").stop(true, true).fadeIn(1000);
+			}
+		// PREV
+		if ($(current_image_thumbnail).prev().length) {
+			$(".prev").css({"left":+left_edge_icon+"px"});
+			var real_left_edge = left_edge_icon - ($(".prev").width()) - 12 ;
+			$(".prev").css({"left":+real_left_edge+"px"});
+			$(".prev").stop(true, true).fadeIn(500);
+		} 
+	}).mouseleave(function() {
+		$(".next, .prev").stop(true, true).fadeOut(200).stop(true, true);
+	});
+});
+
+
+
+
+
+
+
+
+
+
